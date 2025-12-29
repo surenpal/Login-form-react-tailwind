73ce82ccc84ac4ebdce792471497f14bd7eed0a6
surenpal <surenpal666@gmail.com>
2025-12-29 19:24:34 +0900

    bilyanku

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,5 @@
-import LoginPage from "./pages/LoginPage"  
+import LoginPage from "./pages/LoginPage"
 
-
-function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-slate-900">
-      <h1 className="text-4xl font-bold text-white">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad accusamus recusandae nemo! Inventore corporis culpa, animi aliquid voluptatem quia neque! Reprehenderit, quis consequatur beatae porro officiis dicta ab hic quam.
-        Tailwind + Vite + React is working!
-      </h1>
-    </div>
-  )
+export default function App() {
+  return <LoginPage />
 }
-
-export default App
